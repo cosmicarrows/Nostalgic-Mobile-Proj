ea0cbafb626ba56b62f8cda92398dab3d4c7c105
Laurence Wingo <laurence@cosmicarrows.com>
2018-06-14 16:47:37 -0400

    Initial Commit

---
+++ Nostalgic Mobile Proj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nostalgic Mobile Proj
+//
+//  Created by Laurence Wingo on 6/14/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
